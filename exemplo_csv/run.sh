da74acf3ef4138ac30aa9db416a66906b4706cb1
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-29 11:39:49 -0300

    Create run.sh

---
+++ exemplo_csv/run.sh
@@ -0,0 +1,24 @@
+# Liberando a porta no caso de rodar no Cloud9
+sudo service mysql stop
+
+chmod 755 curso_dw.sql
+
+docker-compose up -d
+
+echo ""
+echo "Aguardando a configuração."
+while [ "$(docker logs superset_app 2>&1 | grep "Listening at" | wc -l)" != "1" ]; do
+  printf "."
+  sleep 1
+done
+echo ""
+sh ../config_superset.sh
+echo "Config OK"
+IP=$(curl -s checkip.amazonaws.com)
+echo ""
+echo "URLs do projeto:"
+echo ""
+echo " - PHPMYADMIN         : $IP:8082"
+echo ""
+echo " - SUPERSET UI        : $IP:8088"
+echo ""
